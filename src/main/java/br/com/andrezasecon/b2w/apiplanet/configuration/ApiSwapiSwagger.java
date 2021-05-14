98e33d1693c29536cf438b64cd86c45a213c05a0
andrezasecon <andrezasecon@gmail.com>
2021-05-14 18:11:58 -0300

    Created Swagger

---
+++ src/main/java/br/com/andrezasecon/b2w/apiplanet/configuration/ApiSwapiSwagger.java
@@ -0,0 +1,42 @@
+package br.com.andrezasecon.b2w.apiplanet.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class ApiSwapiSwagger {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.andrezasecon.b2w.apiplanet"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "API  REST Planetas",
+                "API Rest para busca de planetas, consumindo a SWAPI para buscar número de aparições" +
+                        " em filmes para cada planeta",
+                "1.0",
+                "Terms of Service",
+                new Contact("Andreza Secon", "https://www.linkedin.com/in/andreza-secon-b5736788/",
+                        "andrezasecon@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
+
+    }
+}
--- src/main/java/br/com/andrezasecon/b2w/apiplanet/controller/PlanetController.java
+++ src/main/java/br/com/andrezasecon/b2w/apiplanet/controller/PlanetController.java
@@ -8,6 +8,8 @@ import br.com.andrezasecon.b2w.apiplanet.exceptions.InvalidNameException;
 import br.com.andrezasecon.b2w.apiplanet.exceptions.NotFoundNameException;
 import br.com.andrezasecon.b2w.apiplanet.exceptions.PlanetNotFoundException;
 import br.com.andrezasecon.b2w.apiplanet.service.PlanetService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -20,6 +22,8 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api")
 @Validated
+@Api(value = "API  REST Planetas")
+@CrossOrigin(origins = "*")
 public class PlanetController {
 
     @Autowired
@@ -28,37 +32,39 @@ public class PlanetController {
     @Autowired
     private ClientFeignSwapi clientFeignSwapi;
 
-    // Busca todos os planetas na base e o número de filmes na Swapi
+
     @GetMapping
+    @ApiOperation(value = "Retorna uma lista de planetas")
     public List<Planet> findAll() {
         List<Planet> planetList = planetService.findAllPlanets();
         planetList.stream().forEach(p -> {
             PlanetSwapiPaginationResponse planetsApi = clientFeignSwapi.getPlanetsByName(p.getName());
-            planetsApi.getResults().stream().forEach(pa -> {
-                p.setFilmsAppearances(pa.getFilms().size() + p.getFilmsAppearances());
-            });
+            planetsApi.getResults().stream().forEach(swapiPlanet ->
+                    p.setFilmsAppearances(swapiPlanet.getFilms().size() + p.getFilmsAppearances())
+            );
         });
         return planetList;
     }
 
-    // Busca planeta na base de dados pelo id e o número de filmes na Swapi
-    @GetMapping(value = "/{id}")
-    ResponseEntity<Planet> findPlanetById(@PathVariable String id) {
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Retorna um único planeta por id")
+
+    public ResponseEntity<Planet> findPlanetById(@PathVariable String id) {
         if (id.isBlank()) {
             throw new InvalidIdException();
         }
-        return planetService.findPlanetById(id).map(record -> {
-            PlanetSwapiPaginationResponse planetsApi = clientFeignSwapi.getPlanetsByName(record.getName());
-            planetsApi.getResults().stream().forEach(pa -> {
-                record.setFilmsAppearances(pa.getFilms().size() + record.getFilmsAppearances());
-            });
-            return ResponseEntity.ok().body(record);
+        return planetService.findPlanetById(id).map(registry -> {
+            PlanetSwapiPaginationResponse planetsApi = clientFeignSwapi.getPlanetsByName(registry.getName());
+            planetsApi.getResults().stream().forEach(pa ->
+                    registry.setFilmsAppearances(pa.getFilms().size() + registry.getFilmsAppearances())
+            );
+            return ResponseEntity.ok().body(registry);
         }).orElseThrow(() -> new PlanetNotFoundException(id));
     }
 
 
-    // Busca planeta por nome na base dados o número de filmes na Swapi
-    @GetMapping(value = "/find/{name}")
+    @GetMapping("/find/{name}")
+    @ApiOperation(value = "Retorna uma único planeta por nome")
     public List<Planet> findPlanetByName(@PathVariable String name, HttpServletResponse response) {
         if (name.isBlank()) {
             throw new InvalidNameException();
@@ -66,9 +72,9 @@ public class PlanetController {
         List<Planet> planetList = planetService.findByNameIgnoreCase(name);
         planetList.stream().forEach(p -> {
             PlanetSwapiPaginationResponse planetsApi = clientFeignSwapi.getPlanetsByName(p.getName());
-            planetsApi.getResults().stream().forEach(pa -> {
-                p.setFilmsAppearances(pa.getFilms().size() + p.getFilmsAppearances());
-            });
+            planetsApi.getResults().stream().forEach(swapiPlanet ->
+                p.setFilmsAppearances(swapiPlanet.getFilms().size() + p.getFilmsAppearances())
+            );
 
         });
         if (!planetList.isEmpty()) {
@@ -79,15 +85,15 @@ public class PlanetController {
         return planetList;
     }
 
-    // Insere um novo planeta na base de dados
-    @PostMapping
+    @PostMapping("/{id}")
+    @ApiOperation(value = "Insere um planeta")
     public void insertPlanet(@RequestBody @Valid Planet objPlanet, HttpServletResponse response) {
         planetService.insertPlanet(objPlanet);
         response.setStatus(HttpServletResponse.SC_CREATED);
     }
 
-    // Deleta um planeta da base de dados pelo id
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Deleta um produto")
     public void deletePlanet(@PathVariable String id, HttpServletResponse response) {
         if (!id.isBlank()) {
             planetService.deletePlanet(id);
@@ -96,4 +102,5 @@ public class PlanetController {
             throw new InvalidIdException();
         }
     }
+
 }
